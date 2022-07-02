e4e5a6610073f4d14e2a2e77d9f6ddb20e8dbb72
Пригодич Вадим Николаевич <7676562@gmail.com>
2022-07-02 19:09:55 +0300

    fix

--- api.lua
+++ api.lua
@@ -4,11 +4,13 @@ local directory = args[1] .. '.'
 local gamerules = require(directory .. 'gamerules')
 local player = require(directory .. 'player')
 local entity = require(directory .. 'entity')
+local net = require(directory .. 'net')
 
 local api = {
     gamerules=gamerules,
     player=player,
-    entity=entity
+    entity=entity,
+    net=net
 }
 
 return api
---
+++ api/net.lua
@@ -0,0 +1,10 @@
+local net = {}
+
+function net:get(url, key, iduser, params, callback, callbackerror)
+end
+
+function net:post(url, key, iduser, params, callback, callbackerror)
+end
+
+return net
+
